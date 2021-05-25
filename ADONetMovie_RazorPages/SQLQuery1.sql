9457e29b99b44b1092f65b79478baf19a7db02a5
Edem Beytullaev <edem0004@edu.easj.dk>
2021-05-25 23:31:07 +0200

    Boooking

--- ADONetMovie_RazorPages/SQLQuery1.sql
+++ ADONetMovie_RazorPages/SQLQuery1.sql
@@ -1,20 +1,27 @@
 ﻿GO
 
-DROP TABLE [dbo].[Studio];
+
 CREATE TABLE [dbo].[Users] (
-    [Id]   INT          NOT NULL,
+    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     [Name] VARCHAR (50) NULL,
     [Role] VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-DROP TABLE [dbo].[Room]
+GO
 CREATE TABLE [dbo].[Room]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
     [Capacity] INT NOT NULL, 
     [ Size] VARCHAR(50) NOT NULL, 
     [Time] DATETIME NOT NULL, 
     [Status] BIT NOT NULL
+)
+GO
+CREATE TABLE [dbo].[BookingRoom]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [RoomId] INT NOT NULL, 
+    [Time] DATETIME NOT NULL, 
+    [UserID] INT NOT NULL
 )
 
 INSERT INTO [dbo].[Users] ([Id], [Name], [Role]) VALUES (1, N'Edem', N'Admin')
---
+++ ADONetMovie_RazorPages/Services/ADOServices/ADOBookingService/BookingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADONetMovie_RazorPages.Services.ADOServices.ADOBookingService
+{
+    public class BookingService
+    {
+    }
+}
